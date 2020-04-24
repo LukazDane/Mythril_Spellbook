f6e8516baa27a2c7e5fac3d2405286bdc0e88194
Luke <deandrevidal@aol.com>
2020-04-24 13:46:37 -0700

    works on heroku again

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, SelectMultipleField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange
 from app.models import List, Spell
 
 
 class SpellForm(FlaskForm):
     sname = StringField('Spell name', validators=[DataRequired()])
-    slevel = IntegerField('Spell Level', validators=[DataRequired()])
+    slevel = IntegerField('Spell Level', validators=[DataRequired(),NumberRange(0,9) ])
     verbal = BooleanField('Verbal')
     somatic = BooleanField('Somatic')
     material = BooleanField('Material')
